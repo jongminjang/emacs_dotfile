1cdba494d1d220d18e53a219983c321df2bbe8f8
Jongmin Jang <mirth81@gmail.com>
2017-09-13 20:07:43 +0900

    2017 13 Sep

--- init.el
+++ init.el
@@ -23,6 +23,9 @@
 (load "disable-mouse")
 (load "desktop-management")
 
+;; use-package에 빈 config가 있는것은 
+;; config가 있어야만 실행되었음을 알리는 메시지가 나오기 때문에 
+;; caveman debugging을 하기 위해 내가 넣어 둔 것이다.
 (require 'use-package)
 (setq use-package-verbose t)
 
@@ -31,7 +34,6 @@
   :ensure t
   :config
   (progn
-     (print "exec path from shell")
      (add-to-list 'exec-path-from-shell-variables "GOPATH")
 	   (exec-path-from-shell-initialize)
      ))
@@ -76,14 +78,29 @@
      (lambda()
        (set (make-local-variable 'company-backends) '(company-go)))))
   :ensure t
+  :defer t)
+
+;; company for javascript
+(use-package company-tern
+  :ensure t
+  :defer t
+  :init
+  (progn
+    (add-hook 'js-mode-hook (lambda () 
+      (tern-mode t)
+      (add-to-list 'company-backends 'company-tern))))
   )
 
 ;; gometalinter
+;; go get -u gopkg.in/alecthomas/gometalinter.v1
+;; make link gometalinter that refers .v1
+;; gometalinter --install
 (use-package flycheck-gometalinter
   :ensure t
-  :config
+  :defer t
+  :init
   (progn
-    (flycheck-gometalinter-setup)))
+    (add-hook 'go-mode-hook 'flycheck-gometalinter-setup)))
 
 ;; (use-package color-moccur
 ;;   :commands (isearch-moccur isearch-all)
@@ -98,34 +115,33 @@
 
 (use-package flycheck
   :ensure t
+  :defer t
+  :init
+  (progn (add-hook 'prog-mode-hook 'flycheck-mode))
   :config
   (progn
-    (global-flycheck-mode)
-    ;;(add-to-list 'load-path "~/goprojects/src/github.com/dougm/goflymake")
-    ;;(require 'go-flycheck)
+    ;;(global-flycheck-mode)
     ))
 
 ;;; 뭔가 잘못되었을때 helm 이 켜져 있으면 엄청 짜증난다. 마지막에 켜도록 하자
+;; bind 된 명령이 실행되는 시점에 helm이 로딩되고 config가 수행된다.
 (use-package helm
   :ensure t
+  :init (progn
+      (setq helm-buffers-fuzzy-matching t)
+      (helm-mode 1)
+      )
   :bind (("C-x C-f" . helm-find-files)
          ("C-x f" . helm-recentf)
          ("M-y" . helm-show-kill-ring)
-	 ("M-x" . helm-M-x)
-         ("C-x b" . helm-buffers-list))
-  :config (progn
-	    (setq helm-buffers-fuzzy-matching t)
-	    (helm-mode 1)))
+      	 ("M-x" . helm-M-x)
+         ("C-x b" . helm-buffers-list)))
 
 (use-package helm-descbinds
   :ensure t
-  :bind ("C-h b" . helm-descbinds))
-
-(use-package company-tern
-  :ensure t
-  :config (progn
-	    (add-hook 'js-mode-hook (lambda () (tern-mode t)))
-	    (add-to-list 'company-backends 'company-tern)))
+  :bind ("C-h b" . helm-descbinds)
+  :config
+)
 
 (use-package smartparens
   :ensure t
@@ -135,6 +151,8 @@
 	 (show-smartparens-global-mode t))
   :config
   (progn (sp-use-smartparens-bindings))
+  ;; smartparens에서 M-<backspace>가 unwrap 함수를 수행하는건
+  ;; 아무래도 적응이 안된다. 그래서 제거한다.
   :bind (:map smartparens-mode-map ("M-<backspace>" . nil)))
 
 ;;(define-key smartparens-mode-map (kbd "M-<backspace>") nil)
@@ -147,10 +165,6 @@
   :config
   (global-undo-tree-mode))
   
-; (use-package linum-relative
-;   :config 
-;   (helm-linum-relative-mode 1)
-;   )
 
 ;; (show-paren-mode)
 
