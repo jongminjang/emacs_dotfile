f1bfab4736f142e125990ff550beebf290271946
Jongmin Jang <mirth81@gmail.com>
2017-10-18 17:31:02 +0900

    some themes

--- custom.el
+++ custom.el
@@ -4,9 +4,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("cdfc5c44f19211cfff5994221078d7d5549eeb9feda4f595a2fd8ca40467776c" "8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" default)))
  '(package-selected-packages
    (quote
-    (nodejs-repl multiple-cursors-mode expand-region flycheck-gometalinter company-tern helm-descbinds color-moccur smartparens-mode undo-tree web-mode multiple-cursors smartparens magit flycheck exec-path-from-shell company-go company company-mode linum-relative crux helm markdown-mode use-package))))
+    (zenburn-theme monokai-theme nodejs-repl multiple-cursors-mode expand-region flycheck-gometalinter company-tern helm-descbinds color-moccur smartparens-mode undo-tree web-mode multiple-cursors smartparens magit flycheck exec-path-from-shell company-go company company-mode linum-relative crux helm markdown-mode use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
