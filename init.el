9b4cae09008c265ef89785c8bff0219c3b1a5e23
Jongmin Jang <mirth81@gmail.com>
2017-11-01 17:15:30 +0900

    1 Nov 17

--- init.el
+++ init.el
@@ -141,10 +141,29 @@
 (use-package flycheck-gometalinter
   :ensure t
   :defer t
-  :config (progn (setq flycheck-gometalinter-disable-all t))
+  :config
+  ;; (progn (setq flycheck-gometalinter-disable-all t)
+  ;; 		 (flycheck-gometalinter-setup))
+  (flycheck-gometalinter-setup)
   :init
-  (progn
-    (add-hook 'go-mode-hook 'flycheck-gometalinter-setup)))
+  ;; skips 'vendor' directories and sets GO15VENDOREXPERIMENT=1
+  (setq flycheck-gometalinter-vendor t)
+  ;; only show errors
+  (setq flycheck-gometalinter-errors-only t)
+  ;; only run fast linters
+  (setq flycheck-gometalinter-fast t)
+  ;; use in tests files
+  (setq flycheck-gometalinter-test t)
+  ;; disable linters
+  (setq flycheck-gometalinter-disable-linters '("gotype" "gocyclo"))
+  ;; Only enable selected linters
+  (setq flycheck-gometalinter-disable-all t)
+  (setq flycheck-gometalinter-enable-linters '("golint"))
+  ;; Set different deadline (default: 5s)
+  (setq flycheck-gometalinter-deadline "10s")
+  ;; (progn
+  ;;   (add-hook 'go-mode-hook 'flycheck-gometalinter-setup))
+  )
 
 ;; (use-package color-moccur
 ;;   :commands (isearch-moccur isearch-all)
